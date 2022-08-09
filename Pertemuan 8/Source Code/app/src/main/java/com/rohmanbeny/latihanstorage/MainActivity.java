2ef059879664f69a29fa520910301c168f68d8e9
RohmanBenyRiyanto <rendyasw@gmail.com>
2022-08-10 01:18:15 +0700

    Tugas Pertemuan 8

---
+++ Pertemuan 8/Source Code/app/src/main/java/com/rohmanbeny/latihanstorage/MainActivity.java
@@ -0,0 +1,29 @@
+package com.rohmanbeny.latihanstorage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_internal, btn_external;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_internal = (Button) findViewById(R.id.btn_internal);
+        btn_external = (Button) findViewById(R.id.btn_external);
+
+        btn_internal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, InternalActivity.class));
+            }
+        });
+    }
+}
